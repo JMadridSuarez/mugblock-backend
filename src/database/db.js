fdd700dbc1d64a610eee1b0011705e1ee866ee66
Jose Madrid <mikemadrid96@gmail.com>
2024-04-25 15:06:08 -0500

    alv5

--- src/database/db.js
+++ src/database/db.js
@@ -2,12 +2,8 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || "postgres://default:vVRGB2TaujN6@ep-curly-snow-a4a878si-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-    user: process.env.DB_USER ,
-    password:process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    PORT: process.env.PORT,
-    database:process.env.DB,
+    connectionString: process.env.POSTGRES_URL 
+    
     //ssl:true
 })
 module.exports = pool;
