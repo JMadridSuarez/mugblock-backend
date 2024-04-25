dc16730cab0e297986afb493005f45e17ebcaf9e
Jose Madrid <mikemadrid96@gmail.com>
2024-04-25 15:12:40 -0500

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('../src/V1/routes/mainRouter');
+const pool = require('./database/db')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,3 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1', routes);
 
+app.get('ping', async(req,res)=>{
+   const response = await pool.query('SELECT NOW();');
+   res.json({
+    "pong": response.rows[0]
+   })
+})
