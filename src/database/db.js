c2dbc37aea2d23023339c7ff38191dc807480e62
Jose Madrid <mikemadrid96@gmail.com>
2024-04-25 15:55:28 -0500

    new db config

--- src/database/db.js
+++ src/database/db.js
@@ -2,8 +2,12 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    
-    //ssl:true
+    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER ,
+    password:process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    PORT: process.env.PORT,
+    database:process.env.DB,
+    ssl:true
 })
 module.exports = pool;
