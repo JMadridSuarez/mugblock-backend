a833c298bd5d6b66fdc5ccd16af0f9ebe33bb744
Jose Madrid <mikemadrid96@gmail.com>
2024-04-26 14:34:19 -0500

    alvend

--- src/database/db.js
+++ src/database/db.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    user: process.env.DB_USER ,
-    password:process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    PORT: process.env.PORT,
-    database:process.env.DB,
+    
     //ssl:true
 })
 module.exports = pool;
